9a7856f39fc244f5641dc059b5ffa10950780d80
Oleksandr <iskander@kursGit.pl>
2020-07-26 20:03:10 +0200

    new change in file

--- Day_6/company.py
+++ Day_6/company.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import List
 
+
 class Task:
     def __init__(self, description: str, points: int):
         self.description: str = description
@@ -21,21 +22,19 @@ class Employee(ABC):
         self.list_tasks: List[Task] = []
 
     def about(self):
-        return f'Hello, my name is {self.name}, I am {self.age} old.' #opis (zwraca przedstawienie się (imię i wiek))
+        return f'Hello, my name is {self.name}, I am {self.age} old.'  # opis (zwraca przedstawienie się (imię i wiek))
 
     def work(self):
         for task in self.list_tasks:
             if not task.is_done:
-                task[0].execute()
+                task.execute()
 
-    def add_task(self, task:Task):
+    def add_task(self, task: Task):
         self.list_tasks.append(task)
-
 
     @property
     def points(self) -> int:
-        return sum([task.points for task in self.tasks if task.is_done])
-
+        return sum([task.points for task in self.list_tasks if task.is_done])
 
     @property
     def description(self) -> str:
@@ -48,12 +47,12 @@ class SalariedEmployee(Employee):
         self.weekly_salary: float = weekly_salary
 
     @property
-    def mouthly_salary(self) -> float:
+    def monthly_salary(self) -> float:
         return 4 * self.weekly_salary
 
     @property
     def description(self) -> str:
-        return f'{super().description}, I earn {self.mouthly_salary}'
+        return f'{super().description}, I earn {self.monthly_salary}'
 
 
 class HourlyEmployee(Employee):
@@ -63,16 +62,17 @@ class HourlyEmployee(Employee):
         self.count_hours: int = count_hours
 
     @property
-    def mouthly_salary(self) -> float:
-        return self.salary_per_hour * self.count_hours *4
+    def monthly_salary(self) -> float:
+        return self.salary_per_hour * self.count_hours * 4
 
     @property
     def description(self) -> str:
         return f'{super().description}, I work {self.count_hours} hours weekly, {self.salary_per_hour}'
 
-class Company:
-    def __init__(self, name:str):
-        self.name:str = name
+
+class Company():
+    def __init__(self, name: str):
+        self.name: str = name
         self.employees: List[Employee] = []
         self.tasks: List[Task] = []
 
@@ -88,31 +88,22 @@ class Company:
 
     def add_tasks(self, tasks: List[Task]):
         for task in tasks:
-            self.add_tasks(task)
+            self.add_task(task)
 
-    def welcome_emploees(self):
+    def welcome_employees(self):
         return '\n'.join([employee.description for employee in self.employees])
+
     @property
     def count_employees(self):
-        list_employees = self.hire_employee()
+        list_employees = self.employees
         return len(list_employees)
-
-
-    @property
-    def monthly_salary(self) -> float:
-        pass
-
-       
-
-
-
 
 
 
 
 if __name__ == '__main__':
-    #employee_1 = Employee()
-    task = [Task('Buy bananas', 3),Task('Clean Room', 5),Task('Sell Mac', 10)]
+    employee1 = Employee('Anna', 18)
+    task = [Task('Buy bananas', 3), Task('Clean Room', 5), Task('Sell Mac', 10)]
     print(task[0])
     task[0].execute()
     print(task[0].is_done)
@@ -123,7 +114,7 @@ if __name__ == '__main__':
     print(employee_1.description)
     print(employee_2.description)
     employee_1.add_task(Task('Buy 2563', 5))
-    #print(employee_1.tasks)
+    print(employee_1.list_tasks)
     print(employee_1.points)
     employee_1.work()
     print(employee_1.points)
@@ -131,15 +122,26 @@ if __name__ == '__main__':
     employee_1 = SalariedEmployee('Piotr', 25, 1000)
     employee_2 = HourlyEmployee('James', 23, 20, 2000)
     employee_3 = HourlyEmployee('Ewa', 20, 27, 2700)
-
-    tasks = [Task('Buy ananas', 8),Task('Clean Room', 475),Task('Sell Mac', 108),('Buy bananas', 358),Task('Clean Room', 7)]
-    company = Company('Apple')
-    company.add_tasks()
-
-    print(company.welcome_emploees())
-
-    company.count_employees()
-    print(company.count_employees())
-
+    print(employee_1.monthly_salary)
+    print(employee_2.monthly_salary)
+    print(employee_3.monthly_salary)
+    print()
+    print(employee_1.description)
+    print(employee_2.description)
+    print(employee_3.description)
+    print()
 
 
+    company1 = Company('Apple')
+    tasks = [Task('Buy ananas', 8), Task('Clean Room', 475), Task('Sell Mac', 108), Task('Buy bananas', 358),
+             Task('Clean Room', 7)]
+    company1.add_tasks(tasks)
+    print(company1.tasks)
+    employees = [Employee('Anna', 18),
+                 SalariedEmployee('Piotr', 25, 1000),
+                 HourlyEmployee('James', 23, 20, 2000),
+                 HourlyEmployee('Ewa', 20, 27, 2700)]
+
+    company1.hire_employees(employees)
+    print((company1.employees))
+    print(company1.count_employees)
